0605a284fb89122347b4f1e984a64b4070d09f5b
javari <javierarin99@gmail.com>
2026-02-27 00:10:02 +0100

    Hello world!

--- app/src/main.c
+++ app/src/main.c
@@ -3,10 +3,14 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
 	while (1) {
+		LOG_INF("Hello world!");
 		k_sleep(K_MSEC(1000));
 	}
 }
